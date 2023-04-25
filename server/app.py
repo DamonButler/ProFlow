15fdb5ee7312b14765953e10d5eb871dcc65eeb4
Damon <damonbutler1994@gmail.com>
2023-04-25 12:11:06 -0400

    auth stuff

--- server/app.py
+++ server/app.py
@@ -8,13 +8,29 @@ from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
-from models import User, Project, Task
+from models import User, Project, Task, UserProject
 
 class Users(Resource):
     def get(self):
         return "Does this work?"
     
 api.add_resource(Users, '/users')
+
+class Projects(Resource):
+    def get(self):
+        return "Does this work?"
+    
+api.add_resource(Projects, '/projects')
+
+class UserProjects(Resource):
+    def get(self):
+        return "Does this work?"
+
+class Tasks(Resource):
+    def get(self):
+        return "Does this work?"
+    
+api.add_resource(Tasks, '/tasks')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -6,7 +6,6 @@ from datetime import datetime
 from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
-
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -14,8 +13,12 @@ class User(db.Model, SerializerMixin):
     email = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
 
-class Project(db.Model, SerializerMixin):
+    projects = db.relationship('Project', backref='user', lazy=True)
 
+    project_names = association_proxy('projects', 'name')
+
+
+class Project(db.Model, SerializerMixin):
     __tablename__ = 'projects'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -25,10 +28,20 @@ class Project(db.Model, SerializerMixin):
     end_date = db.Column(db.DateTime, nullable=True)
     status = db.Column(db.String, default='Not started', nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    tasks = db.relationship('Task', backref='projects', lazy=True)
+    tasks = db.relationship('Task', backref='project', lazy=True)
+
+    task_names = association_proxy('tasks', 'name')
+
+class UserProject(db.Model):
+    __tablename__ = 'user_projects'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
 
 class Task(db.Model, SerializerMixin):
-
     __tablename__ = 'tasks'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -38,3 +51,14 @@ class Task(db.Model, SerializerMixin):
     end_date = db.Column(db.DateTime, nullable=True)
     status = db.Column(db.String, default='Not started', nullable=False)
     project_id = db.Column(db.Integer, db.ForeignKey('projects.id'), nullable=False)
+
+    @hybrid_property
+    def user(self):
+        return self.project.user
+
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError('Name cannot be empty.')
+        return name
+
